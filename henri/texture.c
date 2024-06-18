ca7414a17c1e09964a1fd9d956c68b0646bc724c
henrifinan <henrifinan33@gmail.com>
2024-06-18 15:37:42 +0200

    ajout fonction play_wiyh_texture_3

--- henri/texture.c
+++ henri/texture.c
@@ -61,6 +61,45 @@ void play_with_texture_1(SDL_Texture *my_texture, SDL_Window *window,
 }
 
 
+void play_with_texture_3(SDL_Texture* my_texture,
+            SDL_Window* window, SDL_Renderer* renderer) {
+    SDL_Rect 
+    source = {0},                             // Rectangle définissant la zone de la texture à récupérer
+    window_dimensions = {0},                  // Rectangle définissant la fenêtre, on n'utilisera que largeur et hauteur
+    destination = {0};                        // Rectangle définissant où la zone_source doit être déposée dans le renderer
+                            
+    SDL_GetWindowSize(                      
+    window, &window_dimensions.w,               
+    &window_dimensions.h);                      // Récupération des dimensions de la fenêtre
+    SDL_QueryTexture(my_texture, NULL, NULL,         
+                        &source.w,                 
+                &source.h);                    // Récupération des dimensions de l'image
+                                
+    /* On décide de déplacer dans la fenêtre         cette image */
+    float zoom = 0.25;                              // Facteur de zoom entre l'image source et l'image affichée
+                                
+    int nb_it = 200;                                // Nombre d'images de l'animation
+    destination.w = source.w * zoom;                // On applique le zoom sur la largeur
+    destination.h = source.h * zoom;                // On applique le zoom sur la hauteur
+    destination.x =                     
+    (window_dimensions.w - destination.w) / 2;  // On centre en largeur
+    float h = window_dimensions.h - destination.h;  // hauteur du déplacement à effectuer
+
+    for (int i = 0; i < nb_it; ++i) {
+        destination.y =
+        h * (1 - exp(-5.0 * i / nb_it) / 2 *
+                (1 + cos(10.0 * i / nb_it * 2 *
+                M_PI)));            // hauteur en fonction du numéro d'image
+
+        SDL_RenderClear(renderer);                    // Effacer l'image précédente
+
+        SDL_SetTextureAlphaMod(my_texture,(1.0-1.0*i/nb_it)*255);      // L'opacité va passer de 255 à 0 au fil de l'animation
+        SDL_RenderCopy(renderer, my_texture, &source, &destination);   // Préparation de l'affichage
+        SDL_RenderPresent(renderer);                  // Affichage de la nouvelle image
+        SDL_Delay(30);                                // Pause en ms
+    }                               
+    //SDL_RenderClear(renderer);                      // Effacer la fenêtre une fois le travail terminé
+}
 
 
 int main(int argc, char** argv) {
