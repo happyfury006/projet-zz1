80295f1f1911ee5b76fbf10a9f9d7185210a397d
henrifinan <henrifinan33@gmail.com>
2024-06-18 20:25:14 +0200

    update photos

--- henri/texture.c
+++ henri/texture.c
@@ -98,10 +98,10 @@ void play_with_texture_4(SDL_Texture* my_texture,
 
     /* Mais pourquoi prendre la totalité de l'image, on peut n'en afficher qu'un morceau, et changer de morceau :-) */
 
-    int nb_images = 8;                     // Il y a 8 vignette dans la ligne de l'image qui nous intéresse
+    int nb_images = 6;                     // Il y a 8 vignette dans la ligne de l'image qui nous intéresse
     float zoom = 2;                        // zoom, car ces images sont un peu petites
     int offset_x = source.w / nb_images,   // La largeur d'une vignette de l'image, marche car la planche est bien réglée
-        offset_y = source.h / 4;           // La hauteur d'une vignette de l'image, marche car la planche est bien réglée
+        offset_y = source.h;           // La hauteur d'une vignette de l'image, marche car la planche est bien réglée
     
 
     state.x = 0 ;                          // La première vignette est en début de ligne
@@ -175,14 +175,8 @@ int main(int argc, char** argv) {
     /* Création du renderer */
     bg = IMG_LoadTexture(renderer,"./images/kanto_route_1.jpg");
     chen0 = IMG_LoadTexture(renderer,"./images/prof-1.png");
-    chen1 = IMG_LoadTexture(renderer,"./images/prof.png");
-    chen2 = IMG_LoadTexture(renderer,"./images/prof1.png");
-    chen3 = IMG_LoadTexture(renderer,"./images/prof2.png");
     if (bg == NULL) end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer);
-    if (chen0 == NULL) end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer);
-    if (chen1 == NULL) end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer);
-    if (chen2 == NULL) end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer);
-    if (chen3 == NULL) end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer);
+    if (perso == NULL) end_sdl(0, "Echec du chargement de l'image dans la texture", window, renderer);
 
     SDL_Rect 
         source = {0},                         // Rectangle définissant la zone de la texture à récupérer
@@ -204,23 +198,44 @@ int main(int argc, char** argv) {
             &source,
             &destination); 
 
-    SDL_QueryTexture(chen0, NULL, NULL,
-                &source.w, &source.h);       // Récupération des dimensions de l'image
+    SDL_QueryTexture(perso,           // Récupération des dimensions de l'image
+            NULL, NULL,
+            &source.w, &source.h);
 
-    float zoom = 0.8;                        // Facteur de zoom à appliquer    
-    destination.w = source.w * zoom;         // La destination est un zoom de la source
-    destination.h = source.h * zoom;         // La destination est un zoom de la source
-    destination.x =
-     (window_dimensions.w - destination.w) /2;     // La destination est au milieu de la largeur de la fenêtre
-    destination.y =
-     (window_dimensions.h - 0.575*window_dimensions.h);
+    /* Mais pourquoi prendre la totalité de l'image, on peut n'en afficher qu'un morceau, et changer de morceau :-) */
 
-    SDL_RenderCopy(renderer, chen0,
-            &source,
-            &destination); 
+    int nb_images = 6;                     // Il y a 8 vignette dans la ligne de l'image qui nous intéresse
+    float zoom = 2;                        // zoom, car ces images sont un peu petites
+    int offset_x = source.w / nb_images,   // La largeur d'une vignette de l'image, marche car la planche est bien réglée
+        offset_y = source.h;           // La hauteur d'une vignette de l'image, marche car la planche est bien réglée
     
 
-                                 
+    state.x = 0 ;                          // La première vignette est en début de ligne
+    state.y = 3 * offset_y;                // On s'intéresse à la 4ème ligne, le bonhomme qui court
+    state.w = offset_x;                    // Largeur de la vignette
+    state.h = offset_y;                    // Hauteur de la vignette
+
+    destination.w = offset_x * zoom;       // Largeur du sprite à l'écran
+    destination.h = offset_y * zoom;       // Hauteur du sprite à l'écran
+
+    destination.y =                        // La course se fait en milieu d'écran (en vertical)
+    (window_dimensions.h - destination.h) /2;
+
+    int speed = 9;
+    for (int x = 0; x < window_dimensions.w - destination.w; x += speed) {
+        destination.x = x;                   // Position en x pour l'affichage du sprite
+        state.x += offset_x;                 // On passe à la vignette suivante dans l'image
+        state.x %= source.w;                 // La vignette qui suit celle de fin de ligne est
+                        // celle de début de ligne
+
+        SDL_RenderClear(renderer);           // Effacer l'image précédente avant de dessiner la nouvelle
+        SDL_RenderCopy(renderer, my_texture, // Préparation de l'affichage
+                &state,
+                &destination);  
+        SDL_RenderPresent(renderer);         // Affichage
+        SDL_Delay(80);                       // Pause en ms
+    }
+                         
     SDL_RenderPresent(renderer); 
     SDL_Delay(3000);                      // Pause exprimée en ms                                   
     
