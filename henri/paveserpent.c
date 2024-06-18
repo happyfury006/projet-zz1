dab1619821cf9505697790574b064f003b4c976b
henrifinan <henrifinan33@gmail.com>
2024-06-18 11:47:00 +0200

    changement de couleur et déplacement fonctionnels

--- henri/paveserpent.c
+++ henri/paveserpent.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 /*********************************************************************************************************************/
 /*                              Programme d'exemple de création de rendu + dessin                                    */
@@ -36,29 +37,47 @@ void end_sdl(char ok,                                               // fin norma
     if (!ok) {                                       // On quitte si cela ne va pas            
     exit(EXIT_FAILURE);                                                  
     }                                                          
-}                                                        
+}      
+
                                     
-void draw(SDL_Renderer* renderer,float w,float h) {  // Je pense que vous allez faire moins laid :)
+void draw(SDL_Renderer* renderer,float w,float h, int color1, int color2) {  // Je pense que vous allez faire moins laid :)
     
-    SDL_Rect rectangle;                                                
-                                        
+    SDL_Rect rectangle;
+    SDL_Rect rectangle2; 
+    SDL_Rect carre; 
+
+                                      
     SDL_SetRenderDrawColor(renderer,                                       
-                200, 0, 0,                             // mode Red, Green, Blue (tous dans 0..255)
+                color1, 0, 0,                             // mode Red, Green, Blue (tous dans 0..255)
                 200);                                 // 0 = transparent ; 255 = opaque
-    rectangle.x = w/2;                                             // x haut gauche du rectangle
-    rectangle.y = h/2;                                                  // y haut gauche du rectangle
-    rectangle.w = 100;                                                // sa largeur (w = width)
-    rectangle.h = 400;                                                // sa hauteur (h = height)
+    rectangle.x = w/2 -40;                                             // x haut gauche du rectangle
+    rectangle.y = h/2 -100;                                                  // y haut gauche du rectangle
+    rectangle.w = 80;                                                // sa largeur (w = width)
+    rectangle.h = 200;
+
+    rectangle2.w = 12;                                                // sa largeur (w = width)
+    rectangle2.h = 20;
+    rectangle2.x = (rectangle.x+(rectangle.w/2))-rectangle2.w/2;                                             // x haut gauche du rectangle
+    rectangle2.y = rectangle.y-rectangle2.h;                                                  // y haut gauche du rectangle
+     
+    carre.h= 60;
+    carre.w=carre.h;
+    carre.x = (rectangle2.x+(rectangle2.w/2))-carre.w/2;
+    carre.y = rectangle2.y-carre.h;                                              // sa hauteur (h = height)
                                         
-    SDL_RenderFillRect(renderer, &rectangle);                        
-                                        
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);                   
-    SDL_RenderDrawLine(renderer,                             
+    SDL_RenderFillRect(renderer, &rectangle);
+    SDL_RenderFillRect(renderer, &carre);
+    SDL_SetRenderDrawColor(renderer, color2, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &rectangle2);                        
+    /*SDL_RenderDrawLine(renderer,                             
             0, 0,                                          // x,y du point de la première extrémité
-            400, 400);                                // x,y seconde extrémité
+            400, 400);     */                                // x,y seconde extrémité
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);                   
+                                  
                                         
 
-    /* tracer un cercle n'est en fait pas trivial, voilà le résultat sans algo intelligent ... */
+    /* tracer un cercle n'est en fait pas trivial, voilà le résultat sans algo intelligent ... 
     for (float angle = 0; angle < 2 * M_PI; angle += M_PI / 4000) {      
         SDL_SetRenderDrawColor(renderer,
                 (cos(angle * 2) + 1) * 255 / 2,          // quantité de Rouge      
@@ -67,9 +86,10 @@ void draw(SDL_Renderer* renderer,float w,float h) {  // Je pense que vous allez 
                 255);                                    // opacité = opaque
         SDL_RenderDrawPoint(renderer,                  
                     200 + 100 * cos(angle),                     // coordonnée en x
-                    200 + 150 * sin(angle));                    //            en y   
-    }
+                    200 + 150 * sin(angle));                    //            en y  
+    }*/ 
 }
+
 
 int main(int argc, char** argv) {
     (void)argc;
@@ -77,6 +97,7 @@ int main(int argc, char** argv) {
 
     SDL_Window* window = NULL;
     SDL_Renderer* renderer = NULL;
+    
 
     SDL_DisplayMode screen;
 
@@ -92,8 +113,8 @@ int main(int argc, char** argv) {
     /* Création de la fenêtre */
     window = SDL_CreateWindow("Premier dessin",
                 SDL_WINDOWPOS_CENTERED,
-                SDL_WINDOWPOS_CENTERED, screen.w * 0.66,
-                screen.h * 0.66,
+                SDL_WINDOWPOS_CENTERED, screen.w,
+                screen.h,
                 SDL_WINDOW_OPENGL);
     if (window == NULL) end_sdl(0, "ERROR WINDOW CREATION", window, renderer);
 
@@ -102,14 +123,80 @@ int main(int argc, char** argv) {
     renderer = SDL_CreateRenderer(window, -1,
                     SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == NULL) end_sdl(0, "ERROR RENDERER CREATION", window, renderer);
+    
+    SDL_Rect rectangle;
+    SDL_Rect rectangle2; 
+    SDL_Rect carre; 
 
-    /*********************************************************************************************************************/
-    /*                                     On dessine dans le renderer                                                   */
-    /*********************************************************************************************************************/
-    /*             Cette partie pourrait avantageusement être remplacée par la boucle évènementielle                     */ 
-    draw(renderer,screen.w,screen.h);                                      // appel de la fonction qui crée l'image  
-    SDL_RenderPresent(renderer);                         // affichage
-    SDL_Delay(1000);                                     // Pause exprimée en ms
+    rectangle.x = screen.w/2 -40;                                             // x haut gauche du rectangle
+    rectangle.y = screen.h/2 -100;                                                  // y haut gauche du rectangle
+    rectangle.w = 80;                                                // sa largeur (w = width)
+    rectangle.h = 200;
+
+    rectangle2.w = 12;                                                // sa largeur (w = width)
+    rectangle2.h = 20;
+    rectangle2.x = (rectangle.x+(rectangle.w/2))-rectangle2.w/2;                                             // x haut gauche du rectangle
+    rectangle2.y = rectangle.y-rectangle2.h;                                                  // y haut gauche du rectangle
+     
+    carre.h= 60;
+    carre.w=carre.h;
+    carre.x = (rectangle2.x+(rectangle2.w/2))-carre.w/2;
+    carre.y = rectangle2.y-carre.h;
+
+    bool bouge = true;
+    bool avant = true;
+    int vitesse = 2;
+    int color1 = 255;
+    int color2 = 120;
+
+
+    while (bouge) {
+        SDL_Event event;
+        if (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) 
+            {
+                bouge = false;
+            } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    // Change la couleur à chaque clique gauche
+                    int temp=color1;
+                    color1=color2;
+                    color2=temp;
+                }
+            }
+        }
+
+        if (avant) {
+            rectangle.x += vitesse;
+            rectangle2.x += vitesse;
+            carre.x += vitesse;
+            if (rectangle.x + rectangle.w >= screen.w) {
+                avant = false;
+            }
+        } else {
+            rectangle.x -= vitesse;
+            rectangle2.x -= vitesse;
+            carre.x -= vitesse;
+            if (rectangle.x <= 0) {
+                avant = true;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, color1, 0, 0, 0);
+        SDL_RenderFillRect(renderer, &rectangle);
+        SDL_RenderFillRect(renderer, &carre);
+        SDL_SetRenderDrawColor(renderer, color2, 0, 0, 0);
+        SDL_RenderFillRect(renderer, &rectangle2);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(20); 
+    }
+
+    SDL_Delay(3000);                                     // Pause exprimée en ms
                                                     
 
     /* on referme proprement la SDL */
