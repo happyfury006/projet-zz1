719639bfbce65c80ebf9f63d4f4ccf30d4b1003d
ValentinTardy <tardy.valentin2003@gmail.com>
2024-06-18 11:08:41 +0200

    Ajout fichiers valentin

---
+++ valentin/paveserpent.c
@@ -0,0 +1,67 @@
+#include <SDL2/SDL.h>
+#include <stdbool.h>
+
+const int WINDOW_LARGEUR = 800;
+const int WINDOW_HAUTEUR = 600;
+const int RECT_TAILLE = 50;
+const int vit = 5;
+
+int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("Animation avec SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_LARGEUR, WINDOW_HAUTEUR, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL_Rect rect = {0, (WINDOW_HAUTEUR - RECT_TAILLE) / 2, RECT_TAILLE, RECT_TAILLE};
+    SDL_Color couleur = {255, 0, 0, 255}; // Couleur initial : rouge
+
+    bool running = true;
+    bool forward = true;
+
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) 
+            {
+                running = false;
+            } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    // Change la couleur à chaque clique gauche
+                    couleur.r = rand() % 256;
+                    couleur.g = rand() % 256;
+                    couleur.b = rand() % 256;
+                }
+            }
+        }
+
+        if (forward) {
+            rect.x += vit;
+            if (rect.x + RECT_TAILLE >= WINDOW_LARGEUR) {
+                forward = false;
+            }
+        } else {
+            rect.x -= vit;
+            if (rect.x <= 0) {
+                forward = true;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, couleur.r, couleur.g, couleur.b, couleur.a);
+        SDL_RenderFillRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(16); 
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
---
+++ valentin/xfenetre.c
@@ -0,0 +1,88 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+// Fonction principale
+int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+    // Dimensions des fenêtres
+    const int windowWidth = 100;
+    const int windowHeight = 100;
+
+    // Initialisation de la SDL
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Erreur d'initialisation de la SDL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Obtenir les dimensions de l'écran
+    SDL_DisplayMode DM;
+    if (SDL_GetCurrentDisplayMode(0, &DM) != 0) {
+        printf("Erreur lors de l'obtention des dimensions de l'écran: %s\n", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    int screenWidth = DM.w;
+    int screenHeight = DM.h;
+
+    // Calcul du nombre de fenêtres nécessaires pour couvrir la diagonale
+    int numWindows = (screenWidth / windowWidth + screenHeight / windowHeight) / 2;
+    SDL_Window* windows[numWindows * 2]; // Deux diagonales
+
+    // Création des fenêtres sur la première diagonale (du haut gauche au bas droit)
+    for (int i = 0; i < numWindows; ++i) {
+        int x = (screenWidth / 2) - (numWindows * windowWidth / 2) + i * windowWidth;
+        int y = (screenHeight / 2) - (numWindows * windowHeight / 2) + i * windowHeight;
+        windows[i] = SDL_CreateWindow("X Window",
+                                      x,
+                                      y,
+                                      windowWidth,
+                                      windowHeight,
+                                      SDL_WINDOW_SHOWN);
+        if (!windows[i]) {
+            printf("Erreur de création de la fenêtre %d: %s\n", i + 1, SDL_GetError());
+            SDL_Quit();
+            return 1;
+        }
+    }
+
+    // Création des fenêtres sur la deuxième diagonale (du haut droit au bas gauche)
+    for (int i = 0; i < numWindows; ++i) {
+        int x = (screenWidth / 2) + (numWindows * windowWidth / 2) - (i + 1) * windowWidth;
+        int y = (screenHeight / 2) - (numWindows * windowHeight / 2) + i * windowHeight;
+        windows[numWindows + i] = SDL_CreateWindow("X Window",
+                                                   x,
+                                                   y,
+                                                   windowWidth,
+                                                   windowHeight,
+                                                   SDL_WINDOW_SHOWN);
+        if (!windows[numWindows + i]) {
+            printf("Erreur de création de la fenêtre %d: %s\n", numWindows + i + 1, SDL_GetError());
+            SDL_Quit();
+            return 1;
+        }
+    }
+
+    // Boucle d'événements
+    int running = 1;
+    SDL_Event event;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = 0;
+            }
+        }
+
+        // Attendre un moment avant de redessiner (optionnel)
+        SDL_Delay(100);
+    }
+
+    // Nettoyage et sortie
+    for (int i = 0; i < numWindows * 2; ++i) {
+        SDL_DestroyWindow(windows[i]);
+    }
+    SDL_Quit();
+
+    return 0;
+}
+
