78dfc6b03703657e89c873a0a0e886e3be3b725d
jusometgar <happyfury006@gmail.com>
2024-06-19 17:46:53 +0200

    petite modif d'indentation

--- jeu1/src/jeu.c
+++ jeu1/src/jeu.c
@@ -5,123 +5,123 @@
 
 
 piece creerPiece(int forme,int player){
-    piece* newpiece = malloc(sizeof(piece));
-    newpiece->formes = forme;
-    newpiece->joueur = player;
+	 piece* newpiece = malloc(sizeof(piece));
+	 newpiece->formes = forme;
+	 newpiece->joueur = player;
 
-    return *newpiece;
+	 return *newpiece;
 }
 
 
 grille creationplateau(){
-   grille plateau ;
-   for (int i = 0; i < 4; i++) {
-      for (int j = 0; j < 4; j++)
-      {
-         plateau.grid[i][j] = NULL;
-      }
-      
-      
-   }
-   return plateau;
+	grille plateau ;
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++)
+		{
+			plateau.grid[i][j] = NULL;
+		}
+		
+		
+	}
+	return plateau;
 }
 
 
 
 void victoireligne(int x,grille plateau){
-   list * lparcouru = NULL;
-   bool victoire = true;
-   int i=0;
-    while (i < 4 && victoire == true)
-    {
-      if (estdanslaliste(plateau.grid[x][i],lparcouru) == 0)
-      {
-         ajoutliste(plateau.grid[x][i],lparcouru);
-      }
-      else
-      {
-         victoire = false;
-      }
-      i++;
-    }
-    if (victoire == true)
-    {
-      printf("Bravo vous avez fait une ligne!\n");
-    }
-    
-    
+	list * lparcouru = NULL;
+	bool victoire = true;
+	int i=0;
+	 while (i < 4 && victoire == true)
+	 {
+		if (estdanslaliste(plateau.grid[x][i],lparcouru) == 0)
+		{
+			ajoutliste(plateau.grid[x][i],lparcouru);
+		}
+		else
+		{
+			victoire = false;
+		}
+		i++;
+	 }
+	 if (victoire == true)
+	 {
+		printf("Bravo vous avez fait une ligne!\n");
+	 }
+	 
+	 
 }
 void victoirecolonne(int y, grille plateau){
-   list * lparcouru = NULL;
-   bool victoire = true;
-   int j=0;
-    while (j < 4 && victoire == true)
-    {
-      if (estdanslaliste(plateau.grid[y][j],lparcouru) == 0)
-      {
-         ajoutliste(plateau.grid[y][j],lparcouru);
-      }
-      else
-      {
-         victoire = false;
-      }
-      j++;
-    }
-    if (victoire == true)
-    {
-      printf("Bravo vous avez fait une colonne!\n");
-    }
-    
+	list * lparcouru = NULL;
+	bool victoire = true;
+	int j=0;
+	 while (j < 4 && victoire == true)
+	 {
+		if (estdanslaliste(plateau.grid[y][j],lparcouru) == 0)
+		{
+			ajoutliste(plateau.grid[y][j],lparcouru);
+		}
+		else
+		{
+			victoire = false;
+		}
+		j++;
+	 }
+	 if (victoire == true)
+	 {
+		printf("Bravo vous avez fait une colonne!\n");
+	 }
+	 
 }
 
 void chercheregion(int x,int y,grille plateau, list * lparcouru){
-   bool victoire = true;
-   int sectionx = x/2;
-   int sectiony = y/2;
-   int subsectionx = x%2;
-   int subsectiony = y%2;
-   for (int i = sectionx; i <subsectionx ; i++)
-   {
-      for (int j = sectiony; j < subsectiony; j++)
-      {
-         if (estdanslaliste(plateau.grid[i][j],lparcouru) == 0)
-         {
-            ajoutliste(plateau.grid[i][j],lparcouru);
-         }
-         else
-         {
-            victoire = false;
-         }  
-      }  
-   }
-   if (victoire == true)
-   {
-      printf("Bravo vous avez fait une region!\n");
-   }
-   
+	bool victoire = true;
+	int sectionx = x/2;
+	int sectiony = y/2;
+	int subsectionx = x%2;
+	int subsectiony = y%2;
+	for (int i = sectionx; i <subsectionx ; i++)
+	{
+		for (int j = sectiony; j < subsectiony; j++)
+		{
+			if (estdanslaliste(plateau.grid[i][j],lparcouru) == 0)
+			{
+				ajoutliste(plateau.grid[i][j],lparcouru);
+			}
+			else
+			{
+				victoire = false;
+			}  
+		}  
+	}
+	if (victoire == true)
+	{
+		printf("Bravo vous avez fait une region!\n");
+	}
+	
 }
 
 void victoireregion(int x, int y, grille plateau){
-    int ix=x%2;
-    int iy=y%2;
-    list* lparcouru = plateau.grid[x][y];
-    chercheregion(ix,iy,plateau,lparcouru);
-      
+	 int ix=x%2;
+	 int iy=y%2;
+	 list* lparcouru = plateau.grid[x][y];
+	 chercheregion(ix,iy,plateau,lparcouru);
+		
 }
 
 
 void victoire(grille plateau, int x, int y){
-   victoireligne(x,plateau);
-   victoirecolonne(y,plateau);
-   // victoireregion();
-   printf("Bravo vous avez gagné !\n");
+	victoireligne(x,plateau);
+	victoirecolonne(y,plateau);
+	// victoireregion();
+	printf("Bravo vous avez gagné !\n");
 }
 
 
 int mainjeu(){;
-   grille plateau = creationplateau();
-   // coupjouer();
-   // victoire(plateau);
-   return 0;
+	grille plateau = creationplateau();
+	// coupjouer();
+	// victoire(plateau);
+	return 0;
 
 }
