7aa2e03f1bf21b67edd66689076a81415f18e6c8
henrifinan <henrifinan33@gmail.com>
2024-06-19 11:36:24 +0200

    ajout fonction creerPiece

--- jeu1/src/jeu.c
+++ jeu1/src/jeu.c
@@ -1,1 +1,11 @@
 #include "jeu.h"
+#include<stdio.h>
+
+piece creerPiece(forme forme,int player){
+    newpiece = malloc(sizeof(piece));
+    newpiece->formes = forme;
+    newpiece->joueur = player;
+    newpiece->x = NULL;
+    newpiece->y = NULL;
+    return 
+}
