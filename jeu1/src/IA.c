e70ddbf4924c95d2ed47c78f448d93e773f33e59
henrifinan <henrifinan33@gmail.com>
2024-06-20 10:21:20 +0200

    fonction genercoup

--- jeu1/src/IA.c
+++ jeu1/src/IA.c
@@ -1,1 +1,36 @@
 #include "IA.h"
+#include "jeu.h"
+#include <math.h>
+
+#define PROFONDEUR 6
+
+arbre generecoup(joueur j,grille plat){
+    int prof = 0;
+    arbre arb= malloc(sizeof(arbre));
+    arb->plateau=plat;
+    for(int i=0; i<4; i++)
+    {
+        if (j->piecerestantes[i][2]>0)
+        {
+            for(int j=0; j<4; j++)
+            {
+                for(int k=0; k<4; k++)
+                {
+                    if(valide(i, j, k, plat->grid))
+                    {
+                        grille platcopie=grillecopie(plat);
+                        applique(platcopie,piecerestantes[i][1],j,k);
+                        inserttete(arb->fils,platecopie);
+                        if (prof<PROFONDEUR)
+                        {
+                            prof+=1;
+                            arb->fils[0]=generecoup(abs(j-1),platcopie);
+                        }
+                        prof-=1;
+                    }
+                }
+            }
+        }
+    }
+    return arb;
+}
--- jeu1/src/IA.h
+++ jeu1/src/IA.h
@@ -1,11 +1,18 @@
 #ifndef GARDIEN_UNIQUE_IA_H
 #define GARDIEN_UNIQUE_IA_H
 
-#include<stdio.h>
+#include <stdio.h>
+#include "jeu.h"
+#define N 4*16
 
 // fichier d'entete classique ou l'on declare les types necessaires,            
 // les fonctions implementees dans le fichier .c                                
 // les declaration de variables globales externes   
+
+typedef struct arbre{       //structure de l'arbre
+    grille* plateau;          //noeud : plateau actualisé
+    arbre fils[N];             //enfants : un tableau de sous arbres
+} arbre;
 
 
 #endif
