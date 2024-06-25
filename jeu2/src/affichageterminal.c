d169a2e96e7434a0c7c27eab17276674b77ae437
jusometgar <happyfury006@gmail.com>
2024-06-25 19:00:36 +0200

    changements indentation des programmes d'affichage

--- jeu2/src/affichageterminal.c
+++ jeu2/src/affichageterminal.c
@@ -10,32 +10,32 @@ void affichagemainjoueur(sitjoueur* joueur){
   for(int i=0; i<NB_RESSOURCE; i++){
     printf(" [%d] ",joueur->mainjoueur[i]);      
     }
-    printf("\n"); 
+  printf("\n"); 
 }
 
 void affichagepioche(sitjoueur* joueur){
   for(int i=0; i<NB_RESSOURCE; i++){
     printf(" [%d] ",joueur->pioche[i]);      
     }
-    printf("\n"); 
+  printf("\n"); 
 }
 void affichagemarchee(sitjoueur* joueur){
   for(int i=0; i<NB_RESSOURCE; i++){
     printf(" [%d] ",joueur->marchee[i]);      
     }
-    printf("\n"); 
+  printf("\n"); 
 }
 void affichagetab_constru_board(sitjoueur* joueur){
   for(int i=0; i<NB_CONSTRUCTION; i++){
     printf(" [%d] ",joueur->tab_constru_board[i]);      
     }
-    printf("\n"); 
+  printf("\n"); 
 }
 void affichagetab_constru_joueur(sitjoueur* joueur){
   for(int i=0; i<NB_CONSTRUCTION; i++){
     printf(" [%d] ",joueur->tab_constru_joueur[i]);      
     }
-    printf("\n"); 
+  printf("\n"); 
 }
 
 void affichage(sitjoueur* joueur1, sitjoueur* joueur2){
