535a15791e813a062ba7c17f854a35ee812c8606
ValentinTardy <tardy.valentin2003@gmail.com>
2024-06-27 16:26:21 +0200

    Test avec clique gauche et clique droit

--- jeu2/src/test.c
+++ jeu2/src/test.c
@@ -1,3 +1,5 @@
+
+
 #include "SDL.h"
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
@@ -18,15 +20,19 @@ void deplacer_texture(SDL_Renderer* renderer, SDL_Texture* background_texture, S
     for (int i = 0; i < steps; ++i) {
         x += increment_x;
         y += increment_y;
+
         SDL_Rect rect_current = { (int)x, (int)y, rect_init.w, rect_init.h };
+
         SDL_RenderClear(renderer);
         SDL_RenderCopy(renderer, background_texture, NULL, NULL);
         SDL_RenderCopy(renderer, texture, NULL, &rect_current);
         SDL_RenderPresent(renderer);
-        SDL_Delay(16); 
+
+        SDL_Delay(16); // Pour créer un effet de mouvement fluide (~60 FPS)
     }
+
     SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, background_texture, NULL, NULL); her le background
+    SDL_RenderCopy(renderer, background_texture, NULL, NULL); // Afficher le background
     SDL_RenderCopy(renderer, texture, NULL, &rect_final);
     SDL_RenderPresent(renderer);
 }
@@ -77,12 +83,14 @@ void echanger_positions(SDL_Renderer* renderer, SDL_Texture* background_texture,
         }
     }
 
+    // Échanger les positions des textures
     SDL_Rect rect1 = rectangles[index_texture1];
     SDL_Rect rect2 = rectangles[index_texture2];
 
     deplacer_texture(renderer, background_texture, textures[index_texture1], rect1, rect2, 5);
     deplacer_texture(renderer, background_texture, textures[index_texture2], rect2, rect1, 5);
 
+    // Mettre à jour les rectangles avec les nouvelles positions
     rectangles[index_texture1] = rect2;
     rectangles[index_texture2] = rect1;
 }
@@ -91,6 +99,8 @@ void echanger_pioche(SDL_Renderer* renderer, SDL_Texture* background_texture, SD
     SDL_Event event;
     bool clicked = false;
     int index_texture1 = -1;
+
+    // Sélectionner la première texture avec un clic droit
     while (!clicked) {
         SDL_WaitEvent(&event);
         if (event.type == SDL_QUIT) {
@@ -110,7 +120,7 @@ void echanger_pioche(SDL_Renderer* renderer, SDL_Texture* background_texture, SD
         }
     }
 
-    int index_texture2 = 1; // index pioche
+    int index_texture2 = 2; // L'index de la pioche est fixe
     SDL_Rect rect_initial1 = {500, 300, 100, 150}; 
     SDL_Rect rect_initial3 = {200, 200, 100, 150}; 
 
@@ -120,11 +130,74 @@ void echanger_pioche(SDL_Renderer* renderer, SDL_Texture* background_texture, SD
 
     
     deplacer_texture(renderer, background_texture, textures[index_texture1], rect1, rect_initial3, 5);
+
+   
     deplacer_texture(renderer, background_texture, textures[index_texture2], rect2, rect_initial1, 5);
 
+    
     rectangles[index_texture1] = rect_initial3;
     rectangles[index_texture2] = rect_initial1;
 }
+
+
+void mouvement_jouer_carte(SDL_Renderer* renderer, SDL_Texture* background_texture, SDL_Texture* textures[], SDL_Rect rectangles[], int nb_textures)
+{
+    SDL_Event event;
+    bool clicked = false;
+    int index_texture1 = -1;
+
+    while (!clicked) {
+        SDL_WaitEvent(&event);
+        if (event.type == SDL_QUIT) {
+            return;
+        } else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
+            int x = event.button.x;
+            int y = event.button.y;
+
+            for (int i = 0; i < nb_textures; ++i) {
+                SDL_Rect rect = rectangles[i];
+                if (x >= rect.x && x <= rect.x + rect.w && y >= rect.y && y <= rect.y + rect.h) {
+                    index_texture1 = i;
+                    clicked = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    int index_texture2 = -1;
+    clicked = false;
+
+    while (!clicked) {
+        SDL_WaitEvent(&event);
+        if (event.type == SDL_QUIT) {
+            return;
+        } else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
+            int x = event.button.x;
+            int y = event.button.y;
+
+            for (int i = 0; i < nb_textures; ++i) {
+                SDL_Rect rect = rectangles[i];
+                if (x >= rect.x && x <= rect.x + rect.w && y >= rect.y && y <= rect.y + rect.h && i != index_texture1) {
+                    index_texture2 = i;
+                    clicked = true;
+                    break;
+                }
+            }
+        }
+    }
+
+    // Échanger les positions des textures
+    SDL_Rect rect1 = rectangles[index_texture1];
+    SDL_Rect rect2 = rectangles[index_texture2];
+
+    deplacer_texture(renderer, background_texture, textures[index_texture2], rect2, rect1, 5);
+
+    // Mettre à jour les rectangles avec les nouvelles positions
+    rectangles[index_texture2] = rect1;
+}
+
+
 
 int main(int argc, char* argv[]) {
     (void)argc;
@@ -152,6 +225,7 @@ int main(int argc, char* argv[]) {
                 quit = true;
             } else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
                 echanger_positions(renderer, background_texture, textures, rectangles, 2);
+               // même clique(trouver 1 solution) mouvement_jouer_carte(renderer,background_texture,textures,rectangles, 3);
             } else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_RIGHT) {
                 echanger_pioche(renderer, background_texture, textures, rectangles, 3);
             }
